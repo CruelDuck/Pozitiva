2957eb493fd0b292171a21155cead0639052f0f4
CruelDuck <jakub.cechlovsky@gmail.com>
2025-08-18 09:33:25 +0200

    Update route.ts [deploy]

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from 'next/server'
 import { put } from '@vercel/blob'
 import { nanoid } from 'nanoid'
 
+
 export const runtime = 'nodejs'
 
 export async function POST(req: Request) {
